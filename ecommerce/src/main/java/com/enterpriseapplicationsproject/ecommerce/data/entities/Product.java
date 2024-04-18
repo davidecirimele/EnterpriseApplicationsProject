0ba33ecb9c5f03c39b15db953f05f7771aea09ed
lorenzopiro <pirolorenzo@live.it>
2024-04-18 18:45:45 +0200

    Aggiunte entità Product e Review Fixato pom logback

---
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Product.java
@@ -0,0 +1,41 @@
+package com.enterpriseapplicationsproject.ecommerce.data.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "CATEGORY")
+    private String category;
+
+    @Column(name = "WEIGHT")
+    private Double weight;
+
+    @Basic(optional = false)
+    @Column(name = "INSERT_DATE")
+    private Date instertDate;
+
+    @Basic(optional = false)
+    @Column(name = "PRICE")
+    private Double price;
+
+    @Basic(optional = false)
+    @Column(name = "STOCK")
+    private Integer stock;
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+    private List<Review> reviews = new ArrayList<>();
+
+}
---
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Review.java
@@ -0,0 +1,31 @@
+package com.enterpriseapplicationsproject.ecommerce.data.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "REVIEW")
+@Data
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "RATING")
+    private Integer rating;
+
+    @Column(name = "COMMENT")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+    private Users user;
+
+}
