dc1f769188465f0957f33e757c6f50d8850d97e9
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-04-20 14:19:58 +0200

    fix oneToMany on Orders

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Product.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Product.java
@@ -1,4 +1,4 @@
-package com.enterpriseapplicationsproject.ecommerce.data.entities;
+package com.enterpriseapplicationsproject.ecommerce.Data.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
