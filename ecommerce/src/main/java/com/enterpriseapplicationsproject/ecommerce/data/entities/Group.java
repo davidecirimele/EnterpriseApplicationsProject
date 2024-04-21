87af4a574e640b78539800651140d2d88609189e
lorenzopiro <pirolorenzo@live.it>
2024-04-21 15:10:33 +0200

    Aggiunta many to many GroupsMemberships tra group e user

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Group.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/Group.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "Groups")
 @Data
@@ -17,4 +20,11 @@ public class Group {
 
     @Column(name = "GROUP_NAME")
     private String groupName;
+
+    @ManyToMany()
+    @JoinTable( name = "GroupsMemberships",
+            joinColumns = @JoinColumn(name = "GROUP_ID"),
+            inverseJoinColumns = @JoinColumn(name = "USER_ID")
+    )
+    private List<User> members;
 }
