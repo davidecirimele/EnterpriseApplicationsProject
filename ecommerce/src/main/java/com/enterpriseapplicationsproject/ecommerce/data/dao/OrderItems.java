287507739ad7676b8acbdc20839e3783d8096b82
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-04-26 14:21:02 +0200

    Repo

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/OrderItems.java
+++
@@ -1,6 +0,0 @@
-package com.enterpriseapplicationsproject.ecommerce.data.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItems extends JpaRepository<OrderItems, Long> {
-}
--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/TransactionsDao.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/TransactionsDao.java
@@ -1,7 +1,16 @@
 package com.enterpriseapplicationsproject.ecommerce.data.dao;
 
+import com.enterpriseapplicationsproject.ecommerce.data.entities.PaymentMethod;
 import com.enterpriseapplicationsproject.ecommerce.data.entities.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransactionsDao extends CrudRepository<Transaction, Long> {
+
+
+
 }
