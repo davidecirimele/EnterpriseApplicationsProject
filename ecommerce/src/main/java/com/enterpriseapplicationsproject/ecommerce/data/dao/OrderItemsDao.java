0d7019fb88b6664bde8e1bc80775bd0100cde612
Samuele Siciliano <142917498+Musa-lee02@users.noreply.github.com>
2024-05-10 15:33:43 +0100

    Update OrderItemsDao.java

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/OrderItemsDao.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/OrderItemsDao.java
@@ -2,5 +2,5 @@ package com.enterpriseapplicationsproject.ecommerce.data.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemsDao extends JpaRepository<OrderItemsDao, Long> {
+public interface OrderItemsDao extends JpaRepository<OrderItem, Long> {
 }
