2a3cf48af0ba0917a6eabe42d72b49fd6d8a06a6
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-05-14 17:23:55 +0200

    fix import

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/OrderItemsDao.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/dao/OrderItemsDao.java
@@ -1,5 +1,6 @@
 package com.enterpriseapplicationsproject.ecommerce.data.dao;
 
+import com.enterpriseapplicationsproject.ecommerce.data.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemsDao extends JpaRepository<OrderItem, Long> {
