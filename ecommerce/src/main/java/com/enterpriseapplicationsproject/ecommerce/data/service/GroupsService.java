5519fa0da476a5d28e36015e699b524db7e786f8
lorenzopiro <pirolorenzo@live.it>
2024-08-07 18:47:19 +0200

    Fix for Groups mapping

--- ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/service/GroupsService.java
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/service/GroupsService.java
@@ -1,4 +1,22 @@
 package com.enterpriseapplicationsproject.ecommerce.data.service;
 
+import com.enterpriseapplicationsproject.ecommerce.dto.GroupDto;
+import com.enterpriseapplicationsproject.ecommerce.dto.WishlistDto;
+
+import java.util.List;
+
 public interface GroupsService {
+    GroupDto createGroup(GroupDto groupDto);
+
+    GroupDto findGroupById(Long id);
+
+    GroupDto updateGroup(Long id, GroupDto groupDto);
+
+    void deleteGroup(Long id);
+
+    List<GroupDto> getAllGroups();
+
+//    void addUserToGroup(Long groupId, Long userId);
+//
+//    void removeUserFromGroup(Long groupId, Long userId);
 }
