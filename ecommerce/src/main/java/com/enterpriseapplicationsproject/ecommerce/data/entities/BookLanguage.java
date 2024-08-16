726ee8c5afcafb1a602d06fae68873450b7d32b5
Musa-lee02 <samuelesiciliano.22@gmail.com>
2024-08-16 17:17:15 +0200

    messi book enum

---
+++ ecommerce/src/main/java/com/enterpriseapplicationsproject/ecommerce/data/entities/BookLanguage.java
@@ -0,0 +1,16 @@
+package com.enterpriseapplicationsproject.ecommerce.data.entities;
+
+public enum BookLanguage {
+    ENGLISH,
+    ITALIAN,
+    FRENCH,
+    GERMAN,
+    SPANISH,
+    PORTUGUESE,
+    RUSSIAN,
+    CHINESE,
+    JAPANESE,
+    KOREAN,
+    ARABIC,
+    HINDI
+}
