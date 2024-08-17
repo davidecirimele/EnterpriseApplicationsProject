d36836b01779ea6097c8345ede76e418a7794567
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-08-17 14:42:34 +0200

    updating https config

--- EcommerceFrontEnd/app/src/main/java/com/example/ecommercefront_end/network/RetrofitClient.kt
+++ EcommerceFrontEnd/app/src/main/java/com/example/ecommercefront_end/network/RetrofitClient.kt
@@ -1,14 +1,39 @@
 package com.example.ecommercefront_end.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object RetrofitClient {
-    private const val BASE_URL = "http://localhost:8080/api/v1/"
+    private const val BASE_URL = "https://192.168.1.29:8443/api/v1/"
+
+    private val client: OkHttpClient
+
+    init {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> = arrayOf()
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+        val allHostsValid = HostnameVerifier { _, _ -> true }
+
+        client = OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier(allHostsValid)
+            .build()
+    }
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BASE_URL).client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
