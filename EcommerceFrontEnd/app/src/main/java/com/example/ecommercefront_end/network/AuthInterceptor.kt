6ca0009b203d02f5c57ac9df347637a0f201d180
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-08-19 17:58:37 +0200

    added interceptor logic

--- EcommerceFrontEnd/app/src/main/java/com/example/ecommercefront_end/network/AuthInterceptor.kt
+++ EcommerceFrontEnd/app/src/main/java/com/example/ecommercefront_end/network/AuthInterceptor.kt
@@ -2,6 +2,10 @@ package com.example.ecommercefront_end.network
 
 import com.example.ecommercefront_end.SessionManager
 import com.example.ecommercefront_end.model.RequiresAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import okhttp3.Interceptor
 import okhttp3.Response
 import retrofit2.Invocation
@@ -11,7 +15,7 @@ class AuthInterceptor(
     private val authApiService: AuthApiService
 ) : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response {
+    override   fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
 
         val requiresAuth = originalRequest.tag(Invocation::class.java)?.method()?.getAnnotation(
@@ -19,44 +23,47 @@ class AuthInterceptor(
 
         if (!requiresAuth) {
 
+            val accessToken = sessionManager.authToken
 
-
-
-        val accessToken = sessionManager.fetchAccessToken()
-
-        val requestBuilder = originalRequest.newBuilder()
-        accessToken?.let {
-            requestBuilder.header("Authorization", "Bearer $it")
-        }
-
-        val requestWithAuth = requestBuilder.build()
-        val response = chain.proceed(requestWithAuth)
-
-        if (response.code() == 401) {
-
-
-            val refreshToken = sessionManager.fetchRefreshToken()
-            if (refreshToken != null) {
-                val newTokensResponse = apiService.refreshToken(refreshToken).execute()
-                if (newTokensResponse.isSuccessful) {
-                    val newAccessToken = newTokensResponse.body()?.accessToken
-                    val newRefreshToken = newTokensResponse.body()?.refreshToken
-
-                    sessionManager.saveAccessToken(newAccessToken!!)
-                    sessionManager.saveRefreshToken(newRefreshToken!!)
-
-                    val newRequest = originalRequest.newBuilder()
-                        .header("Authorization", "Bearer $newAccessToken")
-                        .build()
-
-                    return chain.proceed(newRequest)
-                } else {
-                    // Se il refresh token non funziona, logout o richiedi il login
-                    sessionManager.clearSession()
-                    // Puoi anche lanciare un'eccezione o gestire il logout in altro modo
-                }
+            val requestBuilder = originalRequest.newBuilder()
+            accessToken?.let {
+                requestBuilder.header("Authorization", "Bearer $it")
             }
-        }
+
+            val requestWithAuth = requestBuilder.build()
+            val response = chain.proceed(requestWithAuth)
+
+            if (response.code() == 401) {
+
+
+                    val refreshToken = sessionManager.refreshToken
+                    if (refreshToken != null) {
+                        val newTokensResponse = runBlocking { withContext(Dispatchers.IO) {authApiService.refreshToken(refreshToken)  } }
+                        if (newTokensResponse.isSuccessful) {
+                            val newAccessToken = newTokensResponse.body()?.accessToken
+                            val newRefreshToken = newTokensResponse.body()?.refreshToken
+
+                            if (newAccessToken != null && newRefreshToken != null) {
+                                sessionManager.saveAuthToken(newAccessToken)
+                                sessionManager.saveRefreshToken(newRefreshToken)
+
+                                val newRequest = originalRequest.newBuilder()
+                                    .header("Authorization", "Bearer $newAccessToken")
+                                    .build()
+
+                                response.close()
+
+                                return chain.proceed(newRequest)
+                            } else {
+                                // Se il refresh token non funziona, logout o richiedi il login
+                                sessionManager.clearSession()
+                                // Puoi anche lanciare un'eccezione o gestire il logout in altro modo
+                                //FIXME logout + redirect to login
+                            }
+                        }
+                    }
+                }
+
 
         return response
     }
